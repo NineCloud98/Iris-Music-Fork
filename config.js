c3541942a7b422b78c281903f0e76d9baae673d2
NineCloud98 <ninec9369@gmail.com>
2024-07-31 20:23:33 -0600

    Update config.js

--- config.js
+++ config.js
@@ -31,7 +31,7 @@ module.exports = {
   activityName: "tus lloros", // This is bot status Write Anything here 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://discord.gg/xQF9f9yUEM",
-  CheckmarkIcon: "https://cdn.discordapp.com/attachments/1230824451990622299/1230836684774576168/7762-verified-blue.gif",
+  CheckmarkIcon: "https://cdn.discordapp.com/attachments/1224536147733250098/1268393463427371008/d11ycfu-4298da9d-36f8-4363-a839-0f90f82468e6.gif?ex=66ac42f7&is=66aaf177&hm=c6b3dcc2957b635ac5ddbf48ef3e9e7090cc8c8bfc46a1addd76fa85464505ac&",
   MusicIcon:"https://media.discordapp.net/attachments/1230824451990622299/1236664581364125787/music-play.gif",
   embedTimeout: 5,  // Timeout before the button interaction embeds are deleted ( Default - 5 seconds)
   errorLog: "", 
