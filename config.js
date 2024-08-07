6753ac5d38e665ed4f22e3a23f2db5f5318d0ee9
NineCloud98 <ninec9369@gmail.com>
2024-08-06 19:37:59 -0600

    Update config.js

--- config.js
+++ config.js
@@ -30,7 +30,7 @@ module.exports = {
   musicardTheme:"themes12", //Goes from themes1 to themes19
   activityName: "tus lloros", // This is bot status Write Anything here 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/xQF9f9yUEM",
+  SupportServer: "https://discord.gg/edAr5rgV",
   CheckmarkIcon: "https://cdn.discordapp.com/attachments/1224536147733250098/1268393463427371008/d11ycfu-4298da9d-36f8-4363-a839-0f90f82468e6.gif?ex=66ac42f7&is=66aaf177&hm=c6b3dcc2957b635ac5ddbf48ef3e9e7090cc8c8bfc46a1addd76fa85464505ac&",
   MusicIcon:"https://media.discordapp.net/attachments/1230824451990622299/1236664581364125787/music-play.gif",
   embedTimeout: 5,  // Timeout before the button interaction embeds are deleted ( Default - 5 seconds)
